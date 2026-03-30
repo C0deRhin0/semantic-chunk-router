32e0456846af339b1ee2e92d912da54809c37711
Wilfredo Paulo A. Perez III <180542978+C0deRhin0@users.noreply.github.com>
2026-03-30 20:46:50 +0800

    refactor: expose main modules in package namespace

--- src/semantic_chunk_router/__init__.py
+++ src/semantic_chunk_router/__init__.py
@@ -1,1 +1,6 @@
 __version__ = "0.1.0"
+__all__ = ["SemanticChunker", "SemanticRouter", "MockEmbedder"]
+
+from .chunker import SemanticChunker
+from .router import SemanticRouter
+from .embedder import MockEmbedder
