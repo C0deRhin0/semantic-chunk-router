241246d5796a1124cbcbad089423b60a3308541d
Wilfredo Paulo A. Perez III <180542978+C0deRhin0@users.noreply.github.com>
2026-04-11 14:03:59 +0800

    feat: add RoutingValidationError to error specifications

--- src/semantic_chunk_router/exceptions.py
+++ src/semantic_chunk_router/exceptions.py
@@ -9,3 +9,8 @@ class EmbeddingError(SemanticRouterError):
 class ConfigurationError(SemanticRouterError):
     """Raised when configuration is invalid."""
     pass
+
+
+class RoutingValidationError(SemanticRouterError):
+    """Raised when route definitions are invalid."""
+    pass
